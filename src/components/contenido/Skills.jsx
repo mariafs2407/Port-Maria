0d4e74058f5a6e977674732990a21c6d4668ed37
MariaF <mariafs2407@gmail.com>
2023-02-19 23:08:00 -0500

    Diseño web adaptable -responsive

--- src/components/contenido/Skills.jsx
+++ src/components/contenido/Skills.jsx
@@ -13,7 +13,7 @@ import { FormattedMessage } from 'react-intl';
 const Skills = () => {
     return (
         <div className='cajapadreS' id='skills'>
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+            <svg className='efectoS' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
                 <path fill="#E6D5BB" d="M0,256L48,250.7C96,245,192,235,288,240C384,245,480,267,576,266.7C672,267,768,245,864,240C960,235,1056,245,1152,256C1248,267,1344,277,1392,282.7L1440,288L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path>
             </svg>
             <div className='cajapadre-skill'>
@@ -59,7 +59,7 @@ const Skills = () => {
                     </div>
                 </div>
             </div>
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+            <svg className='efectoS' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
                 <path fill="#E6D5BB" d="M0,128L48,144C96,160,192,192,288,181.3C384,171,480,117,576,117.3C672,117,768,171,864,170.7C960,171,1056,117,1152,106.7C1248,96,1344,128,1392,144L1440,160L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"></path>
             </svg>
         </div>
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -16,7 +16,6 @@ import { langContext } from '../../context/langContext';
 const Header = () => {
 
     const idioma = useContext(langContext);
-    console.log(idioma.establecerLenguaje());
 
     return (
         <div className='contHeader'>
@@ -28,7 +27,7 @@ const Header = () => {
                     <li>
                         <a className='li ' href="#top">
                             <img className='float' src={hogar} />
-                            <FormattedMessage
+                            <FormattedMessage className="text"
                                 id='menu.Home'
                                 defaultMessage="Inicio"
                             />
@@ -38,7 +37,7 @@ const Header = () => {
                     <li>
                         <a className='li ' href="#about">
                             <img className='float' src={perfil} />
-                            <FormattedMessage
+                            <FormattedMessage className="text"
                                 id='menu.About'
                                 defaultMessage="Sobre mí"
                             />
@@ -48,7 +47,7 @@ const Header = () => {
                     <li>
                         <a className='li' href="#skills">
                             <img className='float' src={habilidad} />
-                            <FormattedMessage
+                            <FormattedMessage className="text"
                                 id="menu.Skills"
                                 defaultMessage="Habilidades"
                             />
@@ -58,26 +57,26 @@ const Header = () => {
                     <li>
                         <a className='li ' href="#projects">
                             <img className='float' src={pro} />
-                            <FormattedMessage
+                            <FormattedMessage className="text"
                                 id="menu.Projects"
                                 defaultMessage="Proyectos"
                             />
                         </a>
                     </li>
                     {/* idioma */}
-                    <li>
-                        <button className='icn'
-                            onClick={() => idioma.establecerLenguaje('es-SP')}>
-                            <img src={peru} />
-                        </button>
-                        <button className='icn'
-                            onClick={() => idioma.establecerLenguaje('en-US')}>
-                            <img src={ingles} />
-                        </button>
-                    </li>
                 </ul>
+                <div className='idioma'>
+                    <button className='icn'
+                        onClick={() => idioma.establecerLenguaje('es-SP')}>
+                        <img src={peru} />
+                    </button>
+                    <button className='icn'
+                        onClick={() => idioma.establecerLenguaje('en-US')}>
+                        <img src={ingles} />
+                    </button>
+                </div>
             </div>
-            
+
             <svg className='efecto' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
                 <path fill="#fcb69f" d="M0,96L60,112C120,128,240,160,360,154.7C480,149,600,107,720,74.7C840,43,960,21,1080,26.7C1200,32,1320,64,1380,80L1440,96L1440,0L1380,0C1320,0,1200,0,1080,0C960,0,840,0,720,0C600,0,480,0,360,0C240,0,120,0,60,0L0,0Z"></path>
             </svg>
